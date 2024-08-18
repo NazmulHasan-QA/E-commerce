6c72d443abbbb66f2ade994251269a9333de4b17
Nazmul Hasan <nazmul.steadfastsqa@gmail.com>
2024-08-18 17:52:03 +0600

    Updated

--- src/test/java/Test/Base.java
+++ src/test/java/Test/Base.java
@@ -3,6 +3,7 @@ package Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
@@ -17,7 +18,7 @@ public class Base {
         // WebDriverManager.firefoxdriver().setup();
         WebDriverManager.chromedriver().setup();
 
-        // driver = new FirefoxDriver();
+        //gi driver = new FirefoxDriver();
         driver = new ChromeDriver();
 
         driver.manage().window().maximize();
